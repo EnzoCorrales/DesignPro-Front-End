337d1f4164a145a0fec0cb3c6a169f2b400fcd91
guilledll <guilledll20@gmail.com>
2021-06-12 00:46:12 -0300

    Registro 2

--- src/views/auth/Registro.vue
+++ src/views/auth/Registro.vue
@@ -6,6 +6,12 @@
       <h2 class="title my-0">DesignPro</h2>
       <p class="mt-1 mb-3">Completa el formulario para ingresar</p>
       <form @submit.prevent="register" >
+        <small v-if="error" class="val-error">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+          </svg>
+          {{ error }}
+        </small>
         <input
           v-model="form.Correo"
           type="email"
@@ -37,7 +43,7 @@
           class="input mt-2"
           placeholder="Contraseña"
           required
-          min="6"
+          minlength="6"
         />
         <input
           v-model="form.FNac"
@@ -73,6 +79,7 @@ export default {
         Apellido: "",
         FNac: "",
       },
+      error: "",
     };
   },
   methods: {
@@ -80,7 +87,7 @@ export default {
       auth.register(this.form).then(res => {
         console.log(res);
       }).catch(e => {
-        console.log(e);
+        this.error = e.response.data.Message;
       });
     },
   },
