f814d355383f59048a5efa3f1167cf465c378d24
guilledll <guilledll20@gmail.com>
2021-07-10 18:22:03 -0300

    Proyectos y comentarios

--- src/components/proyectos/Modal.vue
+++ src/components/proyectos/Modal.vue
@@ -41,40 +41,81 @@
         </div>
       </div>
       <!-- CONTENIDO -->
-      <div class="proyect title justify-center p-5">
+      <div class="contenido title justify-center p-5">
+        <img :src="proyect.Portada" class="portada-proyecto" />
+        <h2 class="text-center my-2">{{ proyect.Descripcion }}</h2>
         <div class="contenido-proyecto text-center flex dir-col ma">
-          <div v-for="(proy, i) in proyect.Portafolios" :key="i">
-            <div class="contenido" v-html="proy.Contenido"></div>
-          </div>
+          <div
+            v-for="(proy, i) in proyect.Portafolios"
+            :key="i"
+            v-html="proy.Contenido"
+          ></div>
         </div>
         <div id="info-proyecto text-center">
           <div class="data-proyecto ma text-center">
-            <h3>{{ proyect.Titulo }}</h3>
-            <div class="justify-center flex w-full">
-              <p class="mx-4"><b>Categoria:</b> {{ proyect.Categoria }}</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                />
-              </svg>
-              <p class="flex mx-2">
-                {{ proyect.Visitas }}
-              </p>
+            <h2 class="mb-2 mt-1">{{ proyect.Titulo }}</h2>
+            <div class="justify-center flex align-center w-full mb-1">
+              <div class="flex align-center pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-1"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+                  />
+                </svg>
+                <p class="m-0">
+                  {{ proyect.Likes }}
+                </p>
+              </div>
+              <div class="flex align-center mx-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-1"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  />
+                </svg>
+                <p class="m-0">
+                  {{ proyect.Visitas }}
+                </p>
+              </div>
+              <div class="flex align-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-1"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                  />
+                </svg>
+                <p class="m-0">{{ proyect.CantComentarios }}</p>
+              </div>
             </div>
           </div>
         </div>
@@ -106,7 +147,7 @@
         <hr class="my-4" />
         <!-- SECCION COMENTARIOS -->
         <div class="flex">
-          <div class="mr-3">
+          <div class="mr-3 img-user">
             <img
               v-if="user.ImgPerfil"
               class="rounded-circle h-8 w-8"
@@ -119,7 +160,7 @@
               v-model="form.Contenido"
               class="p-2"
               type="text"
-              rows="10"
+              rows="8"
               name="contenido"
               placeholder="Agrega un comentario..."
             />
@@ -155,6 +196,7 @@
       </div>
       <!-- TAGS -->
       <div class="tags">
+        <strong>Tags:</strong>
         <div class="tag ma">
           <p v-for="(Eti, e) in proyect.Tags" :key="e">
             #<b>{{ Eti.Tag }}</b>
@@ -231,9 +273,8 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5); /* Black background with opacity */
+  background-color: rgba(0, 0, 0, 0.7); /* Black background with opacity */
   z-index: 3; /* Specify a stack order in case you're using a different order for other elements */
-  cursor: pointer; /* Add a pointer on hover */
   overflow-x: hidden;
   overflow-y: overlay;
   .titulo-proyecto {
@@ -253,14 +294,43 @@ export default {
       cursor: pointer;
     }
   }
-  .comentarios {
-    margin-top: 2rem;
+  .contenido {
     background-color: white;
-    padding: 2rem 3rem;
+    margin: auto;
+    margin-bottom: 1.5rem;
+    height: auto;
+    font-family: "Poppins", sans-serif;
+    overflow: hidden;
+    padding: 20px;
+    @media (max-width: 640px) {
+      margin-bottom: 1rem;
+      padding: 10px;
+    }
+    .contenido-proyecto {
+      padding: 10px;
+    }
+    .portada-proyecto {
+      width: 100%;
+      max-height: 600px;
+      margin-bottom: 1rem;
+    }
+    .data-proyecto {
+      margin-top: 10px;
+      border: 1px solid black;
+      padding: 10px;
+    }
+  }
+  .comentarios {
+    margin-top: 1.5rem;
+    background-color: white;
+    padding: 2rem;
     overflow: hidden;
     margin: auto;
     @media (max-width: 640px) {
       padding: 1rem;
+      .img-user {
+        margin-right: 0.5rem;
+      }
     }
     .comentario-text {
       @media (max-width: 640px) {
@@ -269,6 +339,27 @@ export default {
         p {
           font-size: 14px;
         }
+      }
+    }
+  }
+  .tags {
+    margin-top: 1.5rem;
+    background-color: white;
+    text-align: left;
+    margin-bottom: 3rem;
+    padding: 1rem 2rem;
+    @media (max-width: 640px) {
+      padding: 10px;
+    }
+    .tag {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+      p {
+        background-color: whitesmoke;
+        border-radius: 10px;
+        border: solid 0.5px black;
+        padding: 5px 10px;
       }
     }
   }
